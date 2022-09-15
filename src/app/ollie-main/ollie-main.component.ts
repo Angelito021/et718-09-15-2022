f6449d72d35be444acf127b017487d93347fec00
Angelito021 <112655525+Angelito021@users.noreply.github.com>
2022-09-15 15:41:49 -0400

    2 files modified, 4 added

---
+++ src/app/ollie-main/ollie-main.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ollie-main',
+  templateUrl: './ollie-main.component.html',
+  styleUrls: ['./ollie-main.component.css'],
+})
+export class OllieMainComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
